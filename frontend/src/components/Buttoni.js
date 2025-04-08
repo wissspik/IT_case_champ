876771161b77e249fb64c0ba6d3a9ecd17cffb3c
Denis <mosin-denis@bk.ru>
2025-04-08 17:10:12 +0700

    улучшена логика,теперь все идет последовательно добавлен component для упрощение диалога

---
+++ frontend/src/components/Buttoni.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Buttoni({ first, buttons, onClickHandler }) {
+    return (
+        <div className="bot">
+            <p>{first}</p>
+            {buttons.map((buttonText, index) => (
+                <button
+                    key={index}
+                    className="button"
+                    onClick={() => onClickHandler(buttonText)}
+                >
+                    {buttonText}
+                </button>
+            ))}
+        </div>
+    );
+}
