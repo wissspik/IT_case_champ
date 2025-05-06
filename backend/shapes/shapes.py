24606443a03416df8d8cfea4a4ed1a64bed255f5
Руслан <stla7824@gmail.com>
2025-05-06 19:28:26 +0700

    !

--- backend/shapes/shapes.py
+++ backend/shapes/shapes.py
@@ -20,7 +20,7 @@ class DeliveryQuestion(BaseModel):
     category : Literal['exchange','currency'] # доступные категории для вопросов
 
 class BankCommissions(BaseModel):
-    currency : Literal["UZS", "KGS", "BYN","TJS", "RUB", "AMD", "KZT", "AZN", "CNY", "VND", "IRR", "RSD", "AED", "ILS", "GEL", "KGS", "EUR", "KRW", "TRY", "MNT", "MDL", "THB", "IDR", "INR", "PHP"]
+    currency : Literal["UZS", "KGS", "BYN","TJS", "RUB", "AMD", "KZT", "AZN", "CNY", "VND", "IRR", "RSD", "AED", "ILS", "GEL", "KGS", "EUR", "KRW", "TRY", "MNT", "MDL", "THB", "IDR", "INR", "PHP","USD"]
     country: Literal["Узбекистан", "Киргизия", "Беларусь", "Таджикистан",  "Абхазия", "Армения", "Южная Осетия", "Казахстан", "Азербайджан", "Китай",  "Вьетнам", "Иран", "Сербия", "ОАЭ", "Израиль", "Грузия", "Кыргыpcтан", "Кипр", "Греция", "Корея (Республика)", "Турция", "Монголия", "Молдова", "Таиланд", "Приднестровье", "Индонезия", "Индия", "Филиппины"]
     method: Literal['cash','bank_card','FN','mobile','account_number','IBAN','KoronaPay']
     amount : condecimal(max_digits=12, decimal_places=2,ge=0)
